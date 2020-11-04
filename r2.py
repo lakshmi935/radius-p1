4476ee44217b730175e1be297dd402f62ead3483
lakshmi935 <73939006+lakshmi935@users.noreply.github.com>
2020-11-04 17:41:29 +0530

    r2.py
    I have done the project.

---
+++ r2.py
@@ -0,0 +1,6 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:58:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: C:/Users/lenovo/AppData/Local/Programs/Python/Python37/r2.py ====
+3.7994000000000008
+>>> 
